06042e832198fe006251ee085e79686eeaba4eb9
adrianovolter <adriano.j.volter@hotmail.com>
2023-10-03 17:33:28 -0300

    initial commit , configurando db ,iniciando a migrate users_addresses

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -2,27 +2,28 @@ const { config } = require("dotenv");
 config();
 const dbConfig = {
   development: {
+    dialect: "postgres",
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+   
     port: process.env.DB_PORT,
   },
   test: {
+    dialect: "postgres",
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: "database_test",
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
     port: process.env.DB_PORT,
   },
-  production: {
+  production: { 
+    dialect: "postgres",
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: "database_production",
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
     port: process.env.DB_PORT,
   },
 };
