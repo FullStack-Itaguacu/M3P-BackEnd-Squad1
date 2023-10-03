af5d44ce7cd249a1e7f6a5297b95df36537e9aaf
filipebratti <filipebratti@hotmail.com>
2023-10-03 09:58:46 -0300

    add user migration

---
+++ src/database/migrations/01-create-user.js
@@ -0,0 +1,68 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable("users", {
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                unique: true,
+            },
+            full_name: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            cpf: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            birth_date: {
+                type: Sequelize.DATEONLY,
+                allowNull: false,
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true,
+            },
+            phone: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            password: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            created_by: {
+                type: Sequelize.INTEGER,
+                allowNull: true,
+                defaultValue: null,
+            },
+            type_user: {
+                type: Sequelize.ENUM("Administrador", "Buyer"),
+                allowNull: false,
+            },           
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            deleted_at: {
+                type: Sequelize.DATE,
+                allowNull: true,
+            },
+        });
+    },
+
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable("users");
+    },
+}
+
