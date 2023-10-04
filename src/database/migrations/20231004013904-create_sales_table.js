d85e07a770a906210b41dec153bd91cb89faca92
adrianovolter <adriano.j.volter@hotmail.com>
2023-10-04 09:14:03 -0300

    feat: Criando migrate sales , com ajustes feito

--- src/database/migrations/20231004013904-create_sales_table.js
+++ src/database/migrations/20231004013904-create_sales_table.js
@@ -1,74 +1,80 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('sales', {
+    return queryInterface.createTable("sales", {
       id: {
         allowNull: false,
         primaryKey: true,
         autoIncrement: true,
         unique: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       buyer_id: {
         allowNull: false,
         type: Sequelize.INTEGER,
         references: {
-          model: 'users',
-          key: 'id'
-        }
+          model: "users",
+          key: "id",
+        },
       },
       seller_id: {
         allowNull: false,
         type: Sequelize.INTEGER,
         references: {
-          model: 'users',
-          key: 'id'
-        }
+          model: "users",
+          key: "id",
+        },
       },
       product_id: {
         allowNull: false,
         type: Sequelize.INTEGER,
         references: {
-          model: 'products',
-          key: 'id'
-        }
+          model: "products",
+          key: "id",
+        },
       },
       users_addresses_id: {
         allowNull: false,
         type: Sequelize.INTEGER,
         references: {
-          model: 'users_addresses',
-          key: 'id'
-        }
+          model: "users_addresses",
+          key: "id",
+        },
       },
       amount_buy: {
         allowNull: false,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       total: {
         allowNull: false,
-        type: Sequelize.FLOAT
+        type: Sequelize.FLOAT,
       },
       type_payment: {
         allowNull: false,
-        type: Sequelize.ENUM('Cartão de crédito', 'Cartão de débito', 'Pix', 'Boleto', 'Transferência bancária')
+        type: Sequelize.ENUM(
+          "credit_card",
+          "debit_card",
+          "payment_slip",
+          "pix",
+          "transfer"
+        ),
       },
       created_at: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updated_at: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       deleted_at: {
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('sales');
-  }
+    return queryInterface.dropTable("sales");
+  },
 };
