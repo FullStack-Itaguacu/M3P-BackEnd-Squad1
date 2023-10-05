69aa8bb7f5d3914398b7f32c795cc6424dce9884
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2023-10-05 18:34:13 +0000

    ✨️ Feat : Divisao da logica em trechos menores, limpeza de comentarios, correçoes de texto em  varios error.message

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,53 +1,7 @@
-/**
- * Este endpoint deve cadastrar um usuário do tipo BUYER.
-
-Rota usada: POST /user/signup
-
-Dados a serem recebidos:
-
-body:
-
-recebe um objeto user com os dados:
-
-fullname
-cpf
-birthDate
-email
-phone
-password
-E um array address de objetos contendo os campos:
-
-zip
-street
-numberStreet
-neighborhood
-city
-state
-complement
-lat
-long
-Após salvar os dados em suas respectivas tabelas, salvar o id de user e de endereço na tabela users_addresses.
-
-Regras:
-
-Somente os campos complement, lat e lon podem ser nulos
-Os outros campos devem ser obrigatórios, a validação pode ser feita na model de user e address.
-Os campos cpf e email devem ser únicos. Verificar no banco de dados se os valores do campo já existem.
-O campo cpf e phone não pode conter caracteres especiais.
-O campo email deve ser do tipo email.
-O campo de senha deve ser criptografado e ter um mínimo de 8 caracteres, entre eles uma letra maiúscula, minúscula, caracter especial e um número.
-O campo typeUser deve ser somente “BUYER” por default.
-RESPOSTAS:
-
-201: Em caso de sucesso na requisição, retornando um objeto contendo uma mensagem informando que os registros foram criados com sucesso
-422: Quando um campo obrigatório não for preenchido. A validação deve estar tratada por campo.
-409: Exceção de campos duplicados como CPF e EMAIL.
-400: Exceção por campos mal formatados como email, telefone, cpf e senha.
- */
 const User = require("../models/user");
 const Address = require("../models/address");
 require("../models/userAddress");
-const { filtroBodySignUp } = require("../services/user.services");
+const { filtroBodySignUp, errorLauncher, successMessage } = require("../services/user.services");
 
 module.exports = {
   async signUp(req, res) {
@@ -55,55 +9,17 @@ module.exports = {
       const user = req.body.user;
       const addresses = req.body.address;
 
-      await filtroBodySignUp(user, addresses, res);
+      await filtroBodySignUp(user, addresses);
 
       const userCreated = await User.create(user);
       const addressesCreated = await Address.bulkCreate(addresses);
-
-      userCreated.setAddresses(addressesCreated);
-
-      return res.status(201).json({
-        message: `${userCreated.full_name} seja bem vindo a plataforma, cadastramos satisfatoriamente você e seus endereços!`,
-        cause: {
-          new_user: {
-            id: userCreated.id,
-            name: userCreated.full_name,
-            email: userCreated.email,
-          },
-          new_addresses: addressesCreated,
-        },
-        status: 201,
-      });
+      userCreated.setAddresses(addressesCreated)
+      
+      successMessage(res, userCreated, addressesCreated)
+      
+    
     } catch (error) {
-      if (!error.cause) {
-        /*
-        implementar logica para salvar
-        o erro e notificar o dev 
-        na versão 2.0
-        */
-        console.log(error);
-      }
-      //Se o erro for de validação do sequelize ele retorna um erro 422
-      if (error.name === "SequelizeValidationError") {
-        return res.status(400).json({
-          message: error.message,
-          cause:
-            "Requisição mal formatada, verifique os campos obrigatórios e tente novamente",
-          status: 400,
-          error: "BadFormatRequest",
-        });
-      }
-      // Caso o erro não foi tratado no filtroBodySignUp ele retorna um erro 500 com mensagem generica
-      return res.status(error.status || 500).json({
-        message:
-          error.message ||
-          "Ocorreu um erro, pode contatar nosso team de desenvolvimento no email bug_busters_team@dominio.com e enviar esta response nos ficaremos muito gratos em ajudar a resolver o problema, ou verificar a seguente informação para tentar resolver por si mesmo",
-        status: error.status || 500,
-        cause:
-          error.cause ||
-          "Desta vez quem falhou foi o dev, mas não se preocupe, ele já foi notificado e está trabalhando para resolver o problema o mais rápido possível",
-        error: error.name,
-      });
+      errorLauncher(error, res);
     }
   },
 };
--- src/services/user.services.js
+++ src/services/user.services.js
@@ -1,17 +1,15 @@
-const {estaNaBD}= require('./validators')
+const { estaNaBD } = require("./validators");
 const User = require("../models/user");
 
 module.exports = {
-  async filtroBodySignUp(user, addresses, res) {
-
+  async filtroBodySignUp(user, addresses) {
     // Validação de campos obrigatórios do endereço
     if (!addresses) {
-      res.status(422);
       const error = new Error();
         error.name = "NotAddressesRecived",
-        error.message ="Para o bom funcionamento da API, e necessário informar pelo menos um endereço",
-        error.cause = "Não informado endereço na requisição";
-        error.status = 422;
+        error.message = "Para o bom funcionamento da API, e necessário informar pelo menos um endereço",
+        error.cause = "Não foi informado nenhum endereço na requisição";
+      error.status = 422;
 
       throw error;
     }
@@ -28,7 +26,6 @@ module.exports = {
           city === undefined ||
           state === undefined
         ) {
-
           const nao_informado = [];
           if (zip === undefined) nao_informado.push("zip");
           if (street === undefined) nao_informado.push("street");
@@ -37,10 +34,9 @@ module.exports = {
           if (city === undefined) nao_informado.push("city");
           if (state === undefined) nao_informado.push("state");
 
-          res.status(422);
           const error = new Error();
             error.name = "NotFieldsAddressRecived",
-            error.message ="Para o bom funcionamento da API, e necessário informar todos os campos orbigatorios do endereço",
+            error.message = "Para o bom funcionamento da API, e necessário informar todos os campos orbigatorios do endereço",
             error.cause = `Não informado ${nao_informado} na requisição`;
             error.status = 422;
 
@@ -51,35 +47,32 @@ module.exports = {
 
     // Validação de campos obrigatórios do usuário
     const { full_name, cpf, birth_date, email, phone, password } = user;
-    if(!user){
-        res.status(422);
-        const error = new Error();
-            error.name = "NotUserRecived",
-            error.message ="Para o bom funcionamento da API, e necessário informar pelo menos um usuário",
-            error.cause = "Não informado usuário na requisição";
-            error.status = 422;
+    if (!user) {
+      const error = new Error();
+        error.name = "NotUserRecived",
+        error.message = "Para o bom funcionamento da API, e necessário informar pelo menos um usuário",
+        error.cause = "Não informado usuário na requisição";
+      error.status = 422;
 
-        throw error;
+      throw error;
     }
-    if(await estaNaBD(User, 'cpf', cpf)){
-        res.status(409);
-        const error = new Error();
-            error.name = "UserAlreadyExists",
-            error.message ="O usuário já existe na base de dados",
-            error.cause = `CPF ${cpf} já cadastrado`;
-            error.status = 409;
+    if (await estaNaBD(User, "cpf", cpf)) {
+      const error = new Error();
+        error.name = "UserAlreadyExists",
+        error.message = "O usuário já existe na base de dados",
+        error.cause = `CPF ${cpf} já cadastrado`;
+        error.status = 409;
 
-        throw error;
+      throw error;
     }
-    if(await estaNaBD(User, 'email', email)){
-        res.status(409);
-        const error = new Error();
-            error.name = "UserAlreadyExists",
-            error.message ="O usuário já existe na base de dados",
-            error.cause = `EMAIL ${email} já cadastrado`;
-            error.status = 409;
+    if (await estaNaBD(User, "email", email)) {
+      const error = new Error();
+      (error.name = "UserAlreadyExists"),
+        (error.message = "O usuário já existe na base de dados"),
+        (error.cause = `EMAIL ${email} já cadastrado`);
+      error.status = 409;
 
-        throw error;
+      throw error;
     }
 
     if (
@@ -98,14 +91,71 @@ module.exports = {
       if (phone === undefined) nao_informado.push("phone");
       if (password === undefined) nao_informado.push("password");
 
-      res.status(422);
       const error = new Error();
-        error.name = "NotFieldsUserRecived",
-        error.message ="Para o bom funcionamento da API, e necessário informar os seguintes campos:",
+      error.name = "NotFieldsUserRecived",
+        error.message =
+          "Para o bom funcionamento da API, e necessário informar os seguintes campos:",
         error.cause = `Não informado ${nao_informado} na requisição`;
-        error.status = 422;
+      error.status = 422;
 
       throw error;
     }
+  },
+  async errorLauncher(error, res) {
+    if (!error.cause) {
+      /*
+        implementar logica para salvar
+        o erro e notificar o dev 
+        na versão 2.0
+        */
+      console.log(error);
+    }
+    //Se o erro for de validação do sequelize ele retorna um erro 400 por se tratar de uma requisição mal formatada
+    if (error.name === "SequelizeValidationError") {
+      return res.status(400).json({
+        message: error.message,
+        cause:
+          "Requisição mal formatada, verifique os campos obrigatórios e tente novamente",
+        status: 400,
+        error: "BadFormatRequest",
+      });
+    }
+    // Caso o erro não foi tratado no filtroBodySignUp nunca tera error.status, retornamos um erro 500 com mensagem generica
+    return res.status(error.status || 500).json({
+      message:
+        error.message ||
+        "Ocorreu um erro, pode contatar nosso team de desenvolvimento no email bug_busters_team@dominio.com e enviar esta response nos ficaremos muito gratos em ajudar a resolver o problema, ou verificar a seguente informação para tentar resolver por si mesmo",
+      status: error.status || 500,
+      cause:
+        error.cause ||
+        "Desta vez quem falhou foi o dev :(, mas não se preocupe, ele já foi notificado e está trabalhando para resolver o problema o mais rápido possível",
+      error: error.name,
+    });
+  },
+  async successMessage(res, userCreated, addressesCreated) {
+    const addressInfo = addressesCreated.reduce((acc, address) => {
+      acc.push({
+        zip: address.zip,
+        street: address.street,
+        number_street: address.number_street,
+        neighborhood: address.neighborhood,
+        city: address.city,
+        state: address.state,
+      });
+      return acc;
+    }, []);
+
+    return res.status(201).json({
+      message: `${userCreated.full_name} seja bem vindo a plataforma, cadastramos satisfatoriamente você e seus endereços!`,
+      cause: {
+        new_user: {
+          id: userCreated.id,
+          full_name: userCreated.full_name,
+          email: userCreated.email,
+        },
+        new_addresses: addressInfo,
+      },
+      status: 201,
+    });
   },
 };
