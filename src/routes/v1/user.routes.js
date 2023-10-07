470e9859e0224ede7f56e157ee26c5c60ac818e3
filipebratti <filipebratti@hotmail.com>
2023-10-06 22:23:52 -0300

    add tokenValidate and adminValidate

--- src/routes/v1/user.routes.js
+++ src/routes/v1/user.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const {signUp, adminSignUp} = require("../../controllers/user.controller");
+const { tokenValidate, adminValidate } = require("../../services/auth");
 
 class UserRouter {
   routesFromUser() {
     const userRoutes = Router();
     userRoutes.post("/user/login");
     userRoutes.post("/user/signup", signUp);
-    userRoutes.post("/user/admin/signup", adminSignUp);
+    userRoutes.post("/user/admin/signup", tokenValidate, adminValidate, adminSignUp);
     userRoutes.get("/buyers/admin/:offset/:limit");
     userRoutes.get("/buyers/admin/:userId");
     userRoutes.patch("/buyers/admin/:userId");    
