ae7ca97960856573ea02c4459699f1339b61bef0
filipebratti <filipebratti@hotmail.com>
2023-10-07 09:27:56 -0300

    remove authentication token

--- src/routes/v1/user.routes.js
+++ src/routes/v1/user.routes.js
@@ -1,13 +1,12 @@
 const { Router } = require("express");
 const {signUp, adminSignUp} = require("../../controllers/user.controller");
-const { tokenValidate, adminValidate } = require("../../services/auth");
 
 class UserRouter {
   routesFromUser() {
     const userRoutes = Router();
     userRoutes.post("/user/login");
     userRoutes.post("/user/signup", signUp);
-    userRoutes.post("/user/admin/signup", tokenValidate, adminValidate, adminSignUp);
+    userRoutes.post("/user/admin/signup", adminSignUp);
     userRoutes.get("/buyers/admin/:offset/:limit");
     userRoutes.get("/buyers/admin/:userId");
     userRoutes.patch("/buyers/admin/:userId");    
